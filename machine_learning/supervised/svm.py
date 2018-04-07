4b60cd18b7b5f79ec30f78f45f8ace0dc6c5e635
Circle <circle811@gmail.com>
2018-04-07 22:42:01 +0800

    modify svm.py

--- machine_learning/supervised/svm.py
+++ machine_learning/supervised/svm.py
@@ -1,103 +1,172 @@
 import numpy as np
 
+from ..utils.distance import pairwise_l2_distance
 
-def optimize_two(K, Y, C, alpha, g, i, j):
-    # target = const + d1 * delta + 0.5 * d2 * delta ** 2
-    # interval of delta alpha[i]
-    if Y[i] == Y[j]:
-        L = max(-alpha[i], alpha[j] - C)
-        R = min(C - alpha[i], alpha[j])
-    else:
-        L = max(-alpha[i], -alpha[j])
-        R = min(C - alpha[i], C - alpha[j])
-
-    # first and second derivative of alpha[i]
-    d1 = Y[i] * (g[i] - g[j] - Y[i] + Y[j])
-    d2 = K[i, i] + K[j, j] - 2 * K[i, j]
-
-    if d2 < 1e-10:
-        return 0.0, 0.0
-
-    delta = np.clip(-d1 / d2, L, R)
-    return delta, -(d1 * delta + 0.5 * d2 * delta * delta)
+__all__ = ['SVMClassifier']
 
 
-def update_two(K, Y, alpha, g, i, j, delta):
+def optimize(K, Y, C, alpha, g, i):
+    d1 = Y[i] * ((g[i] - Y[i]) - (g - Y))
+    d2 = K[i, i] + K.diagonal() - 2 * K[i]
+
+    delta_u = np.zeros_like(alpha)
+    np.divide(-d1, d2, out=delta_u, where=d2 > 0)
+
+    L = np.maximum(-alpha[i], np.where(Y == Y[i], alpha - C, -alpha))
+    R = np.minimum(C - alpha[i], np.where(Y == Y[i], alpha, C - alpha))
+    delta = np.clip(delta_u, L, R)
+    delta[i] = 0
+
+    descent = -(d1 * delta + 0.5 * d2 * delta * delta)
+    j = np.argmax(descent)
+
+    return j, delta[j], descent[j]
+
+
+def update(K, Y, alpha, g, i, j, delta):
     alpha[i] += delta
-    alpha[j] += -Y[i] * Y[j] * delta
-    g += delta * Y[i] * (K[i, :] - K[j, :])
+    alpha[j] -= Y[i] * Y[j] * delta
+    g += Y[i] * delta * (K[i] - K[j])
+
+
+def compute_b(Y, C, alpha, g):
+    js = np.where((alpha > 0) & (alpha < C))[0]
+    if len(js) == 0:
+        return 0.0
+    else:
+        return np.mean(Y[js] - g[js])
 
 
 def smo(K, Y, C, tol):
     n_samples = K.shape[0]
     alpha = np.zeros(n_samples)
+    g = np.zeros(n_samples)  # g == (alpha * Y) @ K
 
-    # g == np.sum((alpha * Y) * K, axis=1)
-    g = np.zeros(n_samples)
-
-    count = 0
     while True:
-        for i in range(n_samples):
-            best_j = None
-            best_delta = None
-            best_des = -1
-            for j in range(n_samples):
-                if j != i:
-                    delta, des = optimize_two(K, Y, C, alpha, g, i, j)
-                    if best_des < des:
-                        best_j = j
-                        best_delta = delta
-                        best_des = des
-            if best_des > tol:
-                # print('update i={} j={} delta={} des={}'.format(i, best_j, best_delta, best_des))
-                update_two(K, Y, alpha, g, i, best_j, best_delta)
-                count = 0
-            else:
-                count += 1
-                if count >= n_samples:
-                    return alpha
+        s = np.random.randint(n_samples)
+        for t in range(n_samples):
+            i = (s + t) % n_samples
+            j, delta, descent = optimize(K, Y, C, alpha, g, i)
+            if descent > tol:
+                update(K, Y, alpha, g, i, j, delta)
+                break
+        else:
+            return alpha, compute_b(Y, C, alpha, g)
 
 
-def rbf_kernel(X, Z, gamma):
-    if Z is None:
-        Z = X
-    x2 = np.sum(np.square(X), axis=1)
-    z2 = np.sum(np.square(Z), axis=1)
-    d2 = x2[:, None] + z2[None, :] - 2 * X @ Z.T
+def rbf(X0, X1, gamma):
+    d2 = pairwise_l2_distance(X0, X1, square=True)
     return np.exp(-gamma * d2)
 
 
-class SVM:
-    def __init__(self, C=1.0, kernel='rbf', gamma=1.0, tol=1e-5):
+class SVMClassifier:
+    def __init__(self, C=1.0, kernel='rbf', gamma=1.0, tol=1e-4, multi_class='ovr'):
         self.C = C
         self.kernel = kernel
         self.gamma = gamma
         self.tol = tol
-        self.dual_coef = None
-        self.support_vectors = None
-        self.intercept = None
+        self.multi_class = multi_class
         if kernel == 'rbf':
-            self.kernel_func = lambda X, Z: rbf_kernel(X, Z, self.gamma)
+            self.kernel_func = lambda X0, X1: rbf(X0, X1, self.gamma)
         elif kernel == 'linear':
-            self.kernel_func = lambda X, Z: X @ Z.T
+            self.kernel_func = lambda X0, X1: X0 @ X1.T
         else:
-            raise ValueError('unknown kernel')
+            raise ValueError('kernel')
+        self.dual_coef_ = None
+        self.intercept_ = None
+        self.support_vectors_ = None
+        self.classes_ = None
 
     def fit(self, X, Y):
+        self.classes_, Yi = np.unique(Y, return_inverse=True)
+        n_classes = self.classes_.shape[0]
         K = self.kernel_func(X, X)
-        alpha = smo(K, Y, self.C, self.tol)
-        s = np.where(alpha > 0)
-        self.dual_coef = (alpha * Y)[s]
-        self.support_vectors = X[s]
-        j = np.where((alpha > 0) & (alpha < self.C))[0]
-        if len(j) == 0:
-            self.intercept = 0.0
+        if n_classes == 2:
+            y = np.where(Yi == 0, 1, -1)
+            alpha, b = smo(K, y, self.C, self.tol)
+            dual_coef = (y * alpha)[:, np.newaxis]
+            intercept = np.array([b])
+        elif self.multi_class == 'ovr':
+            dual_coef, intercept = self._fit_ovr(K, Yi)
+        elif self.multi_class == 'ovo':
+            dual_coef, intercept = self._fit_ovo(K, Yi)
         else:
-            k1 = self.kernel_func(X[j], self.support_vectors)
-            bs = Y[j] - np.sum(self.dual_coef * k1, axis=1)
-            self.intercept = np.median(bs)
+            raise ValueError('multi_class')
+        sv = np.where(np.any(dual_coef != 0, axis=1))
+        self.dual_coef_ = dual_coef[sv]
+        self.intercept_ = intercept
+        self.support_vectors_ = X[sv]
 
     def predict(self, X):
-        K = self.kernel_func(X, self.support_vectors)
-        scores = np.sum(self.dual_coef * K, axis=1) + self.intercept
-        return np.where(scores < 0, -1, 1)
+        n_classes = self.classes_.shape[0]
+        K = self.kernel_func(X, self.support_vectors_)
+        scores = K @ self.dual_coef_ + self.intercept_
+        if n_classes == 2:
+            return self.classes_[np.where(scores[:, 0] > 0, 0, 1)]
+        elif self.multi_class == 'ovr':
+            return self._predict_ovr(scores)
+        elif self.multi_class == 'ovo':
+            return self._predict_ovo(scores)
+        else:
+            raise ValueError('multi_class')
+
+    def _fit_ovr(self, K, Yi):
+        n_samples = K.shape[0]
+        n_classes = self.classes_.shape[0]
+        dual_coef = np.zeros((n_samples, n_classes))
+        intercept = np.zeros(n_classes)
+        for i in range(n_classes):
+            y = np.where(Yi == i, 1, -1)
+            alpha, b = smo(K, y, self.C, self.tol)
+            dual_coef[:, i] = y * alpha
+            intercept[i] = b
+        return dual_coef, intercept
+
+    def _predict_ovr(self, scores):
+        return self.classes_[np.argmax(scores, axis=1)]
+
+    def _fit_ovo(self, K, Yi):
+        n_samples = K.shape[0]
+        n_classes = self.classes_.shape[0]
+        n_pairs = n_classes * (n_classes - 1) // 2
+        dual_coef = np.zeros((n_samples, n_pairs))
+        intercept = np.zeros(n_pairs)
+        k = 0
+        for i in range(n_classes):
+            for j in range(i + 1, n_classes):
+                pos = np.where(Yi == i)[0]
+                neg = np.where(Yi == j)[0]
+                indexes = np.concatenate([pos, neg])
+                y = np.concatenate([np.ones_like(pos), -np.ones_like(neg)])
+                alpha, neg = smo(K[indexes[:, np.newaxis], indexes], y, self.C, self.tol)
+                dual_coef[indexes, k] = y * alpha
+                intercept[k] = neg
+                k += 1
+        return dual_coef, intercept
+
+    def _predict_ovo(self, scores):
+        n_samples = scores.shape[0]
+        n_classes = self.classes_.shape[0]
+        a = np.arange(n_samples)
+
+        vote = np.zeros((n_samples, n_classes))
+        k = 0
+        for i in range(n_classes):
+            for j in range(i + 1, n_classes):
+                p = np.where(scores[:, k] > 0, i, j)
+                vote[a, p] += 1
+                k += 1
+
+        max_vote = np.max(vote, axis=1)[:, np.newaxis]
+        candidate = vote == max_vote
+
+        vote_c = np.where(candidate, 0, -np.inf)
+        k = 0
+        for i in range(n_classes):
+            for j in range(i + 1, n_classes):
+                s = candidate[:, i] * candidate[:, j] * scores[:, k]
+                vote_c[:, i] += s
+                vote_c[:, j] -= s
+                k += 1
+
+        return self.classes_[np.argmax(vote_c, axis=1)]
