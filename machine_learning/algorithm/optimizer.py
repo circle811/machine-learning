43e1d490a69a9ac103416035410a8f4b273d1097
Circle <circle811@gmail.com>
2018-05-03 10:34:32 +0800

    modify optimizer.py

--- machine_learning/algorithm/optimizer.py
+++ machine_learning/algorithm/optimizer.py
@@ -1,6 +1,8 @@
 import numpy as np
 
-__all__ = ['GD', 'MomentumGD', 'SGD', 'MomentumSGD', 'Adam']
+from .lbfgs import minimize_lbfgs
+
+__all__ = ['GD', 'MomentumGD', 'SGD', 'MomentumSGD', 'Adam', 'LBFGS']
 
 
 class OptimizerBase:
@@ -124,3 +126,41 @@ class Adam(AdamUpdate, SGD):
                  beta_1=0.9, beta_2=0.999, epsilon=1e-08, **kwargs):
         super().__init__(learning_rate=learning_rate, max_iter=max_iter, tol=tol, batch_size=batch_size,
                          beta_1=beta_1, beta_2=beta_2, epsilon=epsilon, **kwargs)
+
+
+class LBFGS(OptimizerBase):
+    def __init__(self, m=20, max_iter=200, bs_max_iter=20, tol=1e-4, g_tol=1e-4, **kwargs):
+        super().__init__(**kwargs)
+        self.m = m
+        self.max_iter = max_iter
+        self.bs_max_iter = bs_max_iter
+        self.tol = tol
+        self.g_tol = g_tol
+        self.keys = None
+        self.slices = None
+
+    def minimize(self, parameter, function_gradient):
+        super().minimize(parameter, function_gradient)
+        self.keys = sorted(parameter.keys())
+        pos = np.add.accumulate([parameter[k].size for k in self.keys])
+        self.slices = [slice(pos[i - 1] if i > 0 else 0, pos[i]) for i in range(len(self.keys))]
+
+    def run(self, **kwargs):
+        x, fs = minimize_lbfgs(self.function_gradient_wrap, self.dict_to_array(self.parameter),
+                               self.m, self.max_iter, self.bs_max_iter, self.tol, self.g_tol)
+        self.set_parameter(x)
+        return fs
+
+    def dict_to_array(self, d):
+        return np.concatenate([d[k].flat for k in self.keys])
+
+    def set_parameter(self, x):
+        for i, k in enumerate(self.keys):
+            s = x[self.slices[i]]
+            d = self.parameter[k]
+            np.copyto(d, s.reshape(d.shape))
+
+    def function_gradient_wrap(self, x):
+        self.set_parameter(x)
+        f, g = self.function_gradient()
+        return f, self.dict_to_array(g)
